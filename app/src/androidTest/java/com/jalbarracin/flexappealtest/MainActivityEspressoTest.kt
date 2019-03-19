1aedcb69d2989bfc972b304b2ff85a9ba8d21800
Juan Albarracin <jalbarracinq@gmail.com>
2019-03-19 18:16:37 +0100

    testing

--- app/src/androidTest/java/com/jalbarracin/flexappealtest/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/jalbarracin/flexappealtest/MainActivityEspressoTest.kt
@@ -1,11 +1,16 @@
 package com.jalbarracin.flexappealtest
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions.close
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.DrawerMatchers.isOpen
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.jalbarracin.flexappealtest.controller.MainActivity
 import org.junit.Rule
@@ -13,21 +18,53 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 
+
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
 
     @get:Rule
     val activity = ActivityTestRule(MainActivity::class.java)
 
+//    @Before
+//    fun launchActivity() {
+//        ActivityScenario.launch(MainActivity::class.java)
+//    }
 
     @Test
-    fun testAdd() {
+    fun clickSideMenuButton_opensDrawerLayout() {
         IdlingRegistry.getInstance().register(activity.activity.countingIdlingResource)
         onView(withId(R.id.sideMenuIconView)).perform(click())
-        pressBack()
+        onView(withId(R.id.drawerLayout)).check(matches(isOpen()))
+        onView(withId(R.id.drawerLayout)).perform(close()).check(matches(isClosed()))
+    }
+
+    @Test
+    fun clickSearchButton_showsSearchEditText() {
+        IdlingRegistry.getInstance().register(activity.activity.countingIdlingResource)
         onView(withId(R.id.searchIconView)).perform(click())
+        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
         onView(withId(R.id.backIconView)).perform(click())
-        onView(withId(R.id.searchIconView)).perform(click())
+//        onView(withId(R.id.searchEditText)).check(matches(isClosed()))
+    }
+
+//    @Test
+//    fun testAdd() {
+//        IdlingRegistry.getInstance().register(activity.activity.countingIdlingResource)
+
+//        onView(withId(R.id.drawerLayout))
+//            .perform(DrawerActions.open())
+//            .check(matches(DrawerMatchers.isOpen()))
+
+//        onView(withId(R.id.sideMenuIconView)).perform(click())
+//        onView(withId(R.id.drawerLayout)).check(ViewAssertions.matches(isOpen()))
+//        pressBack()
+//        onView(withId(R.id.searchIconView)).perform(click())
+
+//        onView(withId(R.id.backIconView)).perform(click())
+//        onView(withId(R.id.searchIconView)).perform(click())
+//        onView(withId(R.id.searchEditText)).perform(replaceText("react"))
+//        onView(withId(R.id.searchIconView)).perform(click())
 
 
 
@@ -42,7 +79,16 @@ class MainActivityEspressoTest {
 //                        ViewMatchers.Visibility.VISIBLE)))
 //        R.id.searchEditText.
 
-    }
+//    }
+
+//    private fun clickOutsideDrawer(
+//        parentDrawerLayout: View, @LayoutRes drawId: Int) {
+//        onView(withId(drawId))
+//            .perform(
+//                CoordinatesClickViewAction.clickOnCoordinates(
+//                    parentDrawerLayout.x + parentDrawerLayout.width + 10,
+//                    parentDrawerLayout.height / 2f))
+//    }
 
 //    @Test
 //    fun intentTest() {
--- app/src/main/java/com/jalbarracin/flexappealtest/controller/MainActivity.kt
+++ app/src/main/java/com/jalbarracin/flexappealtest/controller/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.jalbarracin.flexappealtest.controller
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.test.espresso.idling.CountingIdlingResource
-import com.jalbarracin.flexappealtest.R
 import com.jalbarracin.flexappealtest.controller.adapter.RepositoryAdapter
 import com.jalbarracin.flexappealtest.model.Repository
 import com.jalbarracin.flexappealtest.service.GithubRetrofit
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.include_header.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.jalbarracin.flexappealtest.R.layout.activity_main)
         compositeDisposable = CompositeDisposable()
         configureViews()
         GithubRetrofit.getSearch(this, true)
@@ -48,6 +50,8 @@ class MainActivity : AppCompatActivity() {
                 backIconView.visibility = View.VISIBLE
                 sideMenuIconView.visibility = View.GONE
                 searchEditText.requestFocus()
+                val inputMethodService = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodService.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
             } else {
                 val searchText: String = searchEditText.text.toString()
                 if (searchText.isEmpty()) {
@@ -65,6 +69,8 @@ class MainActivity : AppCompatActivity() {
             searchRelativeLayout.visibility = View.GONE
             backIconView.visibility = View.GONE
             sideMenuIconView.visibility = View.VISIBLE
+            val inputMethodService = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodService.hideSoftInputFromWindow(searchEditText.windowToken, 0)
             if (repositoryAdapter.searchText != null) {
                 repositoryAdapter.clear()
                 repositoryAdapter.searchText = null
